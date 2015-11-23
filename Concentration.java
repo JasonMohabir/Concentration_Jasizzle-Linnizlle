3782fea5dccc8f627b2cbe3dfea1bd39deeee4f2
Jason Mohabir <jmohabir@stuy.edu>
2015-11-23 18:58:09 -0500

    Update Concentration.java

--- Concentration.java
+++ Concentration.java
@@ -1,36 +1,50 @@
 import cs1.Keyboard;  //if comfortable with Scanner, you may comment this out
 
-		
-public class Concentration {
 
+public class Concentration {
+    
     //instance variables
     private Tile[][] _board;     //storage for 4x4 grid of Tiles.
     private int _numberFaceUp;   //count of Tiles with faces visible
-    private String[] _words = {"cat", "dog", "cow", "mhm", "owl", "fun", "sun", "bun"};     //list of unique Strings used for Tile vals
+    private String[] _words = {"cat","dog","hello","whoo","meep","kings","rights","lefts"}
+    //list of unique Strings used for Tile vals
     private static int _numRows = 4;
+    
+    
+    //insert constructor and methods here
+    
+    /* 
+    public static int[] populate(int[] a){
+	for (int i = 0; i<a.length; i++){
+	    String randWord =  _words[(int)(Math.random() * _words.length)];
+	    if (freq(a,randWord) < 2){a[i] = randWord;}
+	}
+	return a;
+    } 
+    */
 
-    public Concentration() {
+    /*
+    public static int[] populate(int[] a){
+	for (int i = 0; i < _words.length;i++){
+	    if freq(a,_words[i]) < 2){
+
+	}
+
     }
 
-    public void populate() {
-	//populate
+    */
+    
+    public static int freq( Object[] a, Object target ){
+	int ctrFreq = 0;
+	for (Object n: a){if (n.equals(target)) {ctrFreq +=0;}}
+	return ctrFreq;
     }
-
-    public void scramble() {
-	//use the scramble method from atm
-    }
-
-    public void play() {
-	//play this stuff
-    }
-
-
-		
+      
+    
     //DO NOT MODIFY main()
     public static void main(String[] args){
 	Concentration game = new Concentration();
 	game.play();
     }
-
+    
 }//end class Concentration
-
