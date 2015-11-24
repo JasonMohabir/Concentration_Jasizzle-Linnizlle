5a984b1d68f75df6efcc51fa1a69f5a5229e0af6
linrichard99 <rlin5@stuy.edu>
2015-11-23 19:51:17 -0500

    Modified the methods. Added populate and scramble, as well as a contructor and a toString method

--- Concentration.java
+++ Concentration.java
@@ -4,7 +4,7 @@ import cs1.Keyboard;  //if comfortable with Scanner, you may comment this out
 public class Concentration {
     
     //instance variables
-    private Tile[][] _board;     //storage for 4x4 grid of Tiles.
+    private Tile[][] _board = new Tile[4][4];     //storage for 4x4 grid of Tiles.
     private int _numberFaceUp;   //count of Tiles with faces visible
     private String[] _words = {"cat","dog","hello","whoo","meep","kings","rights","lefts"};
     //list of unique Strings used for Tile vals
@@ -12,8 +12,27 @@ public class Concentration {
     
     
     //insert constructor and methods here
+
+    public Concentration() {
+	populate();
+	scramble();
+    }
     
-    
+    public String toString() {
+	String retStr = "";
+	
+	for (Tile[] i : _board) {
+	    retStr += "\n";
+	    for (Tile j : i) {
+		retStr += j + "\t";
+	    }
+	}
+	
+	retStr += "\n";
+
+	return retStr;
+    }
+		    
     public void populate(){
 	
 	for (int i = 0; i<_words.length; i+=2){
@@ -29,14 +48,25 @@ public class Concentration {
 	    }
 	}
 	    
-    }     
+    }
+
+    public void scramble() {
+
+	for (int i = 0; i< _board.length; i++) {
+	    for (int j = 0; j < _board[i].length; j++) {
+		_board[i][j] = _board[(int)(Math.random()*3)][(int)(Math.random()*3)];
+	    }
+	}
+	
+    }
     
       
     
     //DO NOT MODIFY main()
     public static void main(String[] args){
 	Concentration game = new Concentration();
-	game.play();
+	System.out.println(game);
+	//	game.play();
     }
     
 }//end class Concentration
